94c34b92f8fbf970440d14a076bfe291724011da
Austin Barr <austinbarr@protonmail.com>
2018-08-22 15:31:10 -0700

    added styles and added svg for user menu

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -10,12 +10,16 @@ import { FirebaseListObservable } from 'angularfire2/database';
   styleUrls: ['./account.component.css'],
   providers: [UserService]
 })
+
+
 export class AccountComponent implements OnInit {
+  
   users: FirebaseListObservable<any[]>;
   constructor(private userService: UserService) { }
 
   ngOnInit() {
     this.users = this.userService.getUserInfo();
   }
+  states = ['AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','MD','MA','MI','MN','MS','MO','MT', 'NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY' ];
 
 }
